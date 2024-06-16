805215e5dc0b4e9b290f912affcba850436bea6e
Veljko-crypto <83712753+Veljko-crypto@users.noreply.github.com>
2024-06-16 18:27:45 +0200

    Delete src directory

--- src/main/java/com/example/demo/controller/ElementRequestToElementMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.controller;
-
-import com.example.demo.service.Element;
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "spring")
-public interface ElementRequestToElementMapper {
-    Element elementRequestToElement(ElementRequest elementRequest);
-}
