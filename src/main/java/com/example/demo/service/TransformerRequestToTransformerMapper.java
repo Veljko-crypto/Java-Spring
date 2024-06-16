273bf215e90795f0d7a728726921314461a7d027
Veljko-crypto <83712753+Veljko-crypto@users.noreply.github.com>
2024-06-17 01:37:18 +0200

    Delete src directory

--- src/main/java/com/example/demo/service/TransformerRequestToTransformerMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.controller.TransformerRequest;
-import org.mapstruct.Mapper;
-
-@Mapper(componentModel = "spring")
-public interface TransformerRequestToTransformerMapper {
-    Transformer transformerRequestToTransformerMapper(TransformerRequest transformerRequest);
-}
